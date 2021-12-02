92541cb031ec59f276f49d19e8b2fcace21ed084
Sam Desborough <sam.desborough@gmail.com>
2021-12-02 13:55:15 +0000

    day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,12 +1,54 @@
 use std::str::FromStr;
 
-enum Command {
+pub enum Command {
     Forward(u32),
     Down(u32),
     Up(u32),
 }
 
-struct ParseCommandError(String);
+struct Position {
+    horizontal: i32,
+    depth: i32, // increases with depth
+    aim: i32,
+}
+
+impl Position {
+    const ORIGIN: Position = Position {
+        horizontal: 0,
+        depth: 0,
+        aim: 0,
+    };
+
+    fn change_aim(&self, adjustment: i32) -> Position {
+        Position {
+            horizontal: self.horizontal,
+            depth: self.depth,
+            aim: self.aim + adjustment,
+        }
+    }
+}
+
+pub fn solve(commands: Vec<Command>) -> i32 {
+    let final_position = commands
+        .iter()
+        .fold(Position::ORIGIN, |pos, command| match command {
+            // increases horizontal position by X units
+            // increases depth by your aim multiplied by X units
+            Command::Forward(units) => Position {
+                horizontal: pos.horizontal + *units as i32,
+                depth: pos.depth + (pos.aim * *units as i32),
+                aim: pos.aim,
+            },
+
+            // increases your aim by X units
+            Command::Down(units) => pos.change_aim(*units as i32),
+
+            // decreases aim by X units
+            Command::Up(units) => pos.change_aim(-(*units as i32)),
+        });
+
+    final_position.horizontal * final_position.depth
+}
 
 impl FromStr for Command {
     type Err = ParseCommandError;
@@ -28,3 +70,6 @@ impl FromStr for Command {
         }
     }
 }
+
+#[derive(Debug)]
+pub struct ParseCommandError(String);
